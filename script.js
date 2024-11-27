df9eebc28d0fb38f5c8595dafdeccbbb5ae9a16c
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-11-26 19:05:14 -0500

    Fix issue with draw results.

--- script.js
+++ script.js
@@ -21,10 +21,12 @@ document.addEventListener('DOMContentLoaded',() => {
 
     btnPaper.addEventListener('click', () => { 
         console.log('you picked paper'); 
+        playGame('paper');
     });
 
     btnScissors.addEventListener('click', () => { 
-        console.log('you picked scissors'); 
+        console.log('you picked scissors');
+        playGame('scissors');
     });
 });
 
@@ -33,6 +35,7 @@ function resetGame() {
     humanScore = 0;
     computerScore = 0;
     currentRound = 0;
+    createScoreboard();
 }
 
 
@@ -89,83 +92,133 @@ function playGame(humanSelection) {
 
     if (currentRound < maxRounds) {
         currentRound++;
+        
     }
+
+    updateScore();
 
     if (currentRound === maxRounds) {
         // end the game - switch to reportResults()
         console.log("Game over! Human: " + humanScore + " Computer: " + computerScore);
         if (humanScore > computerScore) {
             console.log("Human wins!");
-        } else {
+            reportResults('Human');
+        } 
+        if (humanScore < computerScore) {
             console.log("Computer wins!");
+            reportResults('Computer');
+        } 
+        if (humanScore === computerScore) {
+            console.log("It's a draw!");
+            reportResults('No one');
         }
-
-        reportResults(winner)
-
     }
 
 }
 
 
 function reportResults(winner) {
-    const container = document.querySelector('#results');
+    const title = document.querySelector('#gameStatus');
+    const scoreH = document.querySelector('#human');
+    const scoreC = document.querySelector('#computer');
+    const whoWinner = document.querySelector('#gameMsg');
 
-    // <h1>Game Over</h1>
-    // <h3>Human: score</h3>
-    // <h3>Computer: score</h3>
-    // <h2>Winner wins!</h2>
-
-    const title = document.createElement('h1');
-    const scoreH = document.createElement('h3');
-    const scoreC = document.createElement('h3');
-    const whoWinner = document.createElement('h2');
-
-    title.textContent = 'Game over!';
+    title.textContent = 'Game over! ' + winner + ' wins!';
     scoreH.textContent = 'Human: ' + humanScore;
     scoreC.textContent = 'Computer: ' + computerScore;
-    whoWinner.textContent = winner + ' wins!';
+    // whoWinner.textContent = '';
+}
 
 
-    container.appendChild(title);
-    container.appendChild(scoreH);
-    container.appendChild(scoreC);
-    container.appendChild(whoWinner);
+function updateScore() {
+    const title = document.querySelector('#gameStatus');
+    const scoreH = document.querySelector('#human');
+    const scoreC = document.querySelector('#computer');
+
+    title.textContent = 'Round: ' + currentRound;
+    scoreH.textContent = 'Human: ' + humanScore;
+    scoreC.textContent = 'Computer: ' + computerScore;
+}
+
+
+function createScoreboard() {
+    const container = document.querySelector('#results');
+
+    if (!container.hasChildNodes()) {
+        const title = document.createElement('h1');
+        const scoreH = document.createElement('h3');
+        const scoreC = document.createElement('h3');
+        const whoWinner = document.createElement('h2');
+    
+        title.setAttribute('id', 'gameStatus');
+        scoreH.setAttribute('id', 'human');
+        scoreC.setAttribute('id', 'computer');
+        whoWinner.setAttribute('id', 'gameMsg');
+    
+        title.textContent = 'Round: ' + currentRound;
+        scoreH.textContent = 'Human: ' + humanScore;
+        scoreC.textContent = 'Computer: ' + computerScore;
+        whoWinner.textContent = '';
+    
+        container.appendChild(title);
+        container.appendChild(scoreH);
+        container.appendChild(scoreC);
+        container.appendChild(whoWinner);
+    } else {
+
+        const title = document.querySelector('#gameStatus');
+        const scoreH = document.querySelector('#human');
+        const scoreC = document.querySelector('#computer');
+        const whoWinner = document.querySelector('#gameMsg');
+
+        title.textContent = 'Round: ' + currentRound;
+        scoreH.textContent = 'Human: ' + humanScore;
+        scoreC.textContent = 'Computer: ' + computerScore;
+        whoWinner.textContent = '';
+    }
+
+}
+
+
+function updateGameMsg(message) {
+    const whoWinner = document.querySelector('#gameMsg');
+    whoWinner.textContent = message;
 }
 
 
 function playRound(humanChoice, computerChoice) {
 
     if (humanChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "rock") {
-        console.log("Draw! Rock doesn't do anything to Rock");
+        updateGameMsg("Draw! Rock doesn't do anything to Rock");
     }
     if (humanChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "paper") {
-        console.log("Draw! Paper doesn't do anything to Paper");
+        updateGameMsg("Draw! Paper doesn't do anything to Paper");
     }
     if (humanChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "scissors") {
-        console.log("Draw! Scissors don't do anything to Scissors");
+        updateGameMsg("Draw! Scissors don't do anything to Scissors");
     }
     if (humanChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "paper") {
-        console.log("You lose! Paper wraps Rock");
+        updateGameMsg("You lose! Paper wraps Rock");
         return "computer";
     }
     if (humanChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "scissors") {
-        console.log("You win! Rock smashes Scissors");
+        updateGameMsg("You win! Rock smashes Scissors");
         return "human";
     }
     if (humanChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "rock") {
-        console.log("You win! Paper wraps Rock");
+        updateGameMsg("You win! Paper wraps Rock");
         return "human";
     }
     if (humanChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "scissors") {
-        console.log("You lose! Scissors cuts Paper");
+        updateGameMsg("You lose! Scissors cuts Paper");
         return "computer";
     }
     if (humanChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "paper") {
-        console.log("You win! Scissors cuts Paper");
+        updateGameMsg("You win! Scissors cuts Paper");
         return "human";
     }
     if (humanChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "rock") {
-        console.log("You lose! Rock smashes Scissors");
+        updateGameMsg("You lose! Rock smashes Scissors");
         return "computer";
     }
 
